1784016308cd2130b200e22201ce1e1c62441d27
ArronVan <duelestneo445@gmail.com>
2020-05-02 17:53:49 -0400

    create initial calendar list

--- src/Budget.js
+++ src/Budget.js
@@ -7,22 +7,22 @@ class Budget extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            budget: 500.05,
+            budget: 500,
             selectedDate: Date.now(),
             dates: [
                 {date: new Date(2020, 2, 25),
                  increases: [{description: "Add Money 1", value: 100}, {description: "Add Money 2", value: 200.5}],
-                 decreases: [{description: "Pay Money 1", value: 20}]
+                 decreases: [{description: "Pay Money 1", value: 20.3}]
                 },
-                {date: new Date(2020, 4, 1),
-                 increases: [{description: "Add Money 1", value: 300}],
+                {date: new Date(2020, 4, 11),
+                 increases: [{description: "Add Money 1", value: 300.02}],
                  decreases: [{description: "Pay Money 1", value: 500}]
                 }
             ]
         };
 
         this.addDate = this.addDate.bind(this);
-        this.getBudgetAmountOnCurrentDate = this.getBudgetAmountOnCurrentDate.bind(this);
+        this.getBudgetAmountOnDate = this.getBudgetAmountOnDate.bind(this);
         this.changeSelectedDate = this.changeSelectedDate.bind(this);
     }
 
@@ -44,14 +44,14 @@ class Budget extends React.Component {
         }));
     }
 
-    getBudgetAmountOnCurrentDate() {
+    getBudgetAmountOnDate(date) {
         let amount = this.state.budget;
 
         var i;
         for (i = 0; i < this.state.dates.length; i++) {
             let indexDate = this.state.dates[i];
 
-            if (isBefore(indexDate.date, this.state.selectedDate) || isSameDay(indexDate.date, this.state.selectedDate)) {
+            if (isBefore(indexDate.date, date) || isSameDay(indexDate.date, date)) {
                 var j;
                 for (j = 0; j < indexDate.increases.length; j++) {
                     amount += indexDate.increases[j].value;
@@ -62,7 +62,7 @@ class Budget extends React.Component {
             }
         }
 
-        return amount;
+        return amount.toFixed(2);
     }
 
     render() {
@@ -71,10 +71,18 @@ class Budget extends React.Component {
                 <p>
                     Selected Date: {getMonthName(this.state.selectedDate)} {getDate(this.state.selectedDate)}, {getYear(this.state.selectedDate)}
                     <br/>
-                    Budget: {this.getBudgetAmountOnCurrentDate()}
+                    Budget: {this.getBudgetAmountOnDate(this.state.selectedDate)}
                 </p>
                 <button onClick={this.addDate}>Click</button>
-                <Calendar budget={this.state.budget} changeSelectedDate={this.changeSelectedDate} dates={this.state.dates} addDate={this.addDate2} ref="calendar" />
+                <Calendar
+                    budget={this.state.budget}
+                    selectedDate={this.state.selectedDate}
+                    changeSelectedDate={this.changeSelectedDate}
+                    getBudgetAmountOnDate={this.getBudgetAmountOnDate}
+                    dates={this.state.dates}
+                    addDate={this.addDate2}
+                    ref="calendar"
+                />
             </>
         );
     };
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,11 @@
-import React from 'react';
+import React from 'react'
 import { getAllDaysInMonth, getDayOfWeekName } from './DateFunctions'
+import { FixedSizeList as List } from 'react-window'
+import { getDate, isSameDay } from 'date-fns'
+
+import './Calendar.css'
 
 class Calendar extends React.Component {
-    constructor(props) {
-        super(props);
-
-        console.log(getAllDaysInMonth(Date.now()));
-        this.onClick = this.onClick.bind(this);
-    }
-    onClick() {
-        this.props.changeSelectedDate(new Date(2020, 3, 1));
-    }
-
     getBudgetValueOnDay(budget, additions, subtractions) {
         let value = budget;
 
@@ -26,25 +20,41 @@ class Calendar extends React.Component {
 
     render() {
         let budget = this.props.budget;
+        let allDaysInMonth = getAllDaysInMonth(this.props.selectedDate);
+
         return (
             <>
-                <button onClick={this.onClick}>Click2</button>
+                <button onClick={() => this.props.changeSelectedDate(new Date(2020, 3, 1))}>Click2</button>
                 <ul>
-                {this.props.dates.map((day, index) => {
-                    return <li key={index}>
-                        Date: {getDayOfWeekName(day.date)} - Budget: {budget = this.getBudgetValueOnDay(budget, day.increases, day.decreases)} - Additions:[{day.increases.map((inc) => {
-                            return <>
-                                {inc.description}: {inc.value}&nbsp;
-                            </>;
-                        })}]
-                        - Subtractions[{day.decreases.map((dec) => {
-                            return <>
-                                {dec.description}: {dec.value}&nbsp;
-                            </>
-                        })}]
-                    </li>
-                })}
+                    {this.props.dates.map((day, index) => {
+                        return <li key={index}>
+                            Date: {getDayOfWeekName(day.date)} - Budget: {budget = this.getBudgetValueOnDay(budget, day.increases, day.decreases)} - Additions:[{day.increases.map((inc) => {
+                                return <>
+                                    {inc.description}: {inc.value}&nbsp;
+                                </>;
+                            })}]
+                            - Subtractions[{day.decreases.map((dec) => {
+                                return <>
+                                    {dec.description}: {dec.value}&nbsp;
+                                </>
+                            })}]
+                        </li>
+                    })}
                 </ul>
+                <List
+                    height={500}
+                    width={500}
+                    itemSize={60}
+                    itemCount={allDaysInMonth.length}
+                >
+                    {({ index, style }) => (
+                        <div style={style}
+                            onClick={() => this.props.changeSelectedDate(allDaysInMonth[index])}
+                            className={isSameDay(allDaysInMonth[index], this.props.selectedDate) ? "CalendarListSelected" : "CalendarList"}>
+                            {getDayOfWeekName(allDaysInMonth[index])} {getDate(allDaysInMonth[index])}: {this.props.getBudgetAmountOnDate(allDaysInMonth[index])}
+                        </div>
+                    )}
+                </List>
             </>
         );
     }

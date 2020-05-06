31d8ba9be90f7b1ff21563ff800ca6fe0199ef4a
ArronVan <duelestneo445@gmail.com>
2020-05-06 11:35:32 -0400

    added input error handling

--- src/InputFields.js
+++ src/InputFields.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NumberFormat from 'react-number-format'
 import Radio from '@material-ui/core/Radio'
 import Button from '@material-ui/core/Button'
+import WarningIcon from '@material-ui/icons/Warning';
 
 import './InputFields.css'
 
@@ -13,7 +14,10 @@ class InputFields extends React.Component {
             showAddTransaction: false,
             transactionDescription: "",
             currencyValue: null,
-            isAdd: true
+            isAdd: true,
+            showBudgetError: false,
+            showTransactionNameError: false,
+            showTransactionValueError: false
         };
 
         this.changeBudget = this.changeBudget.bind(this);
@@ -33,6 +37,12 @@ class InputFields extends React.Component {
     }
 
     changeBudget() {
+        if (this.state.currencyValue == null || !this.state.currencyValue > 0)
+        {
+            this.setState({showBudgetError: true});
+            return;
+        }
+
         this.props.changeBudget(this.state.currencyValue);
         this.props.createNotification("success", "Initial Balance Changed", "Your initial balance has been changed to $" + this.state.currencyValue.toFixed(2) + "!");
         this.setState({currencyValue: null});
@@ -40,6 +50,24 @@ class InputFields extends React.Component {
     }
 
     addTransaction() {
+        var isValid = true;
+        if (this.state.transactionDescription == null || this.state.transactionDescription.replace(/\s/g,"") === "") {
+            isValid = false;
+            this.setState({showTransactionNameError: true});
+        }
+        else
+            this.setState({showTransactionNameError: false});
+        
+        if (this.state.currencyValue == null || !this.state.currencyValue > 0) {
+            isValid = false;
+            this.setState({showTransactionValueError: true});
+        }
+        else
+            this.setState({showTransactionValueError: false});
+        
+        if (!isValid)
+            return;
+
         this.props.addTransactionOnSelectedDate(this.state.currencyValue, this.state.transactionDescription, this.state.isAdd);
         this.props.createNotification("success", "Transaction Added", "Your transaction has been successfully added!");
         this.setState({transactionDescription: "", currencyValue: null, isAdd: true});
@@ -47,29 +75,30 @@ class InputFields extends React.Component {
     }
 
     showChangeBudget() {
-        if (!this.state.showChangeBudget)
-            this.setState({transactionDescription: "", currencyValue: null, isAdd: true});
-
+        this.closeAll();
         this.setState({showChangeBudget: true});
-        this.setState({showAddTransaction: false});
 
         document.addEventListener('click', this.handleOutsideClick, false);
     }
 
     showAddTransaction() {
-        if (!this.state.showAddTransaction)
-            this.setState({transactionDescription: "", currencyValue: null, isAdd: true});
-
+        this.closeAll();
         this.setState({showAddTransaction: true});
-        this.setState({showChangeBudget: false});
 
         document.addEventListener('click', this.handleOutsideClick, false);
     }
 
     closeAll() {
-        this.setState({transactionDescription: "", currencyValue: null, isAdd: true});
-        this.setState({showChangeBudget: false});
-        this.setState({showAddTransaction: false});
+        this.setState({
+            transactionDescription: "",
+            currencyValue: null,
+            isAdd: true,
+            showChangeBudget: false,
+            showAddTransaction: false,
+            showBudgetError: false,
+            showTransactionNameError: false,
+            showTransactionValueError: false
+        });
 
         document.removeEventListener('click', this.handleOutsideClick, false);
     }
@@ -82,6 +111,9 @@ class InputFields extends React.Component {
                 <div className="InputField">
                     {this.state.showChangeBudget ?
                     <>
+                        Enter the value that you want your new initial budget to be.
+                        <br/>
+                        <WarningIcon style={{color: 'red', opacity: (this.state.showBudgetError ? 1 : 0)}}/>
                         <NumberFormat
                           value={this.state.currencyValue}
                           thousandSeparator={true}
@@ -91,20 +123,34 @@ class InputFields extends React.Component {
                           decimalScale={2}
                           onValueChange={(values) => { this.setState({currencyValue: values.floatValue}); }}
                           className="InputFieldInput"
+                          style={{marginTop: '10px', borderColor: (this.state.showBudgetError ? 'red' : 'initial')}}
                         />
                         <br/>
-                        <Button style={{marginTop: '30px', marginRight: '10px'}} variant="contained" color="secondary" onClick={this.closeAll}>Cancel</Button>
-                        <Button style={{marginTop: '30px', marginLeft: '10px'}} variant="contained" color="primary" onClick={this.changeBudget}>Change</Button>
+                        {this.state.showBudgetError ?
+                        <div style={{color: 'red'}}>Value must be greater than $0.</div>
+                        : ''}
+                        <Button style={{marginTop: '10px', marginRight: '10px'}} variant="contained" color="secondary" onClick={this.closeAll}>Cancel</Button>
+                        <Button style={{marginTop: '10px', marginLeft: '10px'}} variant="contained" color="primary" onClick={this.changeBudget}>Change</Button>
                     </> : ''}
                     {this.state.showAddTransaction ?
                     <>
+                        Enter the name of your new transaction, its value, and if it should increase or decrease your overall budget.
+                        <br/>
+                        <WarningIcon style={{color: 'red', opacity: (this.state.showTransactionNameError ? 1 : 0)}}/>
                         <input
                           type="text"
                           className="InputFieldInput"
-                          placeholder="Enter Description"
+                          placeholder="Enter Name"
                           value={this.state.transactionDescription}
                           onChange={(event) => this.setState({transactionDescription: event.target.value})}
+                          style={{marginTop: '10px', borderColor: (this.state.showTransactionNameError ? 'red' : 'initial')}}
                         />
+                        <div style={{color: 'red'}}>
+                            {this.state.showTransactionNameError ?
+                            'Name field cannot be empty.' : ''}
+                        </div>
+                        <br/>
+                        <WarningIcon style={{color: 'red', opacity: (this.state.showTransactionValueError ? 1 : 0)}}/>
                         <NumberFormat
                           value={this.state.currencyValue}
                           thousandSeparator={true}
@@ -114,21 +160,27 @@ class InputFields extends React.Component {
                           decimalScale={2}
                           onValueChange={(values) => { this.setState({currencyValue: values.floatValue}); }}
                           className="InputFieldInput"
+                          style={{borderColor: (this.state.showTransactionValueError ? 'red' : 'initial')}}
                         />
+                        <div style={{color: 'red'}}>
+                            {this.state.showTransactionValueError ?
+                            'Value must be greater than $0.' : ''}
+                        </div>
+                        <div style={{marginTop: '10px'}}>
+                            <Radio
+                            checked={!this.state.isAdd}
+                            onChange={() => this.setState({isAdd: !this.state.isAdd})}
+                            value={false}
+                            /> Decreasing
+                            <Radio
+                            checked={this.state.isAdd}
+                            onChange={() => this.setState({isAdd: !this.state.isAdd})}
+                            value={true}
+                            /> Increasing
+                        </div>
                         <br/>
-                        <Radio
-                          checked={!this.state.isAdd}
-                          onChange={() => this.setState({isAdd: !this.state.isAdd})}
-                          value={false}
-                        /> Decreasing
-                        <Radio
-                          checked={this.state.isAdd}
-                          onChange={() => this.setState({isAdd: !this.state.isAdd})}
-                          value={true}
-                        /> Increasing
-                        <br/>
-                        <Button style={{marginTop: '30px', marginRight: '10px'}} variant="contained" color="secondary" onClick={this.closeAll}>Cancel</Button>
-                        <Button style={{marginTop: '30px', marginLeft: '10px'}} variant="contained" color="primary" onClick={this.addTransaction}>Add</Button>
+                        <Button style={{marginRight: '10px'}} variant="contained" color="secondary" onClick={this.closeAll}>Cancel</Button>
+                        <Button style={{marginLeft: '10px'}} variant="contained" color="primary" onClick={this.addTransaction}>Add</Button>
                     </> : ''}
                 </div>
             </div>
